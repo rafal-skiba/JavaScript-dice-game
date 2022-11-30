44a732b188b31b644ac1e2cd2e9e6c252f06cd70
Rafał Skiba <rr.skiba92@gmail.com>
2022-11-30 23:46:20 +0100

    fixed bug with new-game button

--- script.js
+++ script.js
@@ -15,10 +15,30 @@ const bigScore1 = document.querySelector("#score--1");
 bigScore0.textContent = 0;
 bigScore1.textContent = 0;
 
-const scores = [0,0];
-let score = 0;
-let activePlayer = 0;
-let play = true;
+
+    let scores;
+    let score;
+    let activePlayer;
+    let play;    
+
+    const startState = function () {
+        bigScore0.textContent = 0;
+        bigScore1.textContent = 0;
+        current_0.textContent = 0;
+        current_1.textContent = 0;
+        section0.classList.add("player--active");
+        section1.classList.remove("player--active");
+        section0.classList.remove("player--winner");
+        section1.classList.remove("player--winner");
+        dice.classList.add("hidden");
+        scores = [0,0];
+        score = 0;
+        activePlayer = 0;
+        play = true;   
+
+    }
+
+    startState();
 
 
 const switchPlayer = function () {
@@ -35,6 +55,7 @@ btnRoll.addEventListener("click", ()=> {
         const randomNumber = Math.trunc(Math.random()*6)+1
         dice.classList.remove("hidden");
         dice.src = `dice-${randomNumber}.png`;
+        
         if (randomNumber !== 1) {
             score += randomNumber;
             document.querySelector(`#current--${activePlayer}`).textContent = score;
@@ -64,13 +85,4 @@ btnHold.addEventListener("click", () => {
     }
 })
 
-btnNew.addEventListener("click", () => {
-    bigScore0.textContent = 0;
-    bigScore1.textContent = 0;
-    play = true;
-    current_0.textContent = 0;
-    current_1.textContent = 0;
-    document.querySelector(`.player--${activePlayer}`).classList.remove("player--winner");
-    scores[activePlayer] = 0;
-    score = 0;
-})
+btnNew.addEventListener("click", startState)
